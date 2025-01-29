7cee8574f484a83099a88020aaadd53c4234bd61
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-01-29 11:25:55 -0300

    Update sample package with `PrivateAssets` to remove double reference in the test project.

---
+++ ricaun.ILRepack.Sample.Tests/ReferenceTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ricaun.ILRepack.Sample.Tests
+{
+    public class ReferenceTests
+    {
+        [Test]
+        public void Test()
+        {
+            _ = typeof(Newtonsoft.Json.JsonConverter);
+            _ = typeof(System.Text.Json.JsonSerializer);
+        }
+    }
+}
